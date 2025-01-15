86cea52b263c766829a7f343139bfb197bebafa3
LA_Чиркин А.В. <LA_ChirkinAV@gtp.tn.corp>
2025-01-15 17:51:07 +0300

    modified settings.py

--- news_portal/news_portal/settings.py
+++ news_portal/news_portal/settings.py
@@ -165,5 +165,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.yandex.ru'  # адрес сервера Яндекс-почты для всех один и тот же
 EMAIL_PORT = 465  # порт smtp сервера тоже одинаковый
 EMAIL_HOST_USER = 'chirkin.extra@yandex.ru'  # ваше имя пользователя, например, если ваша почта user@yandex.ru, то сюда надо писать user, иными словами, это всё то что идёт до собаки
-EMAIL_HOST_PASSWORD = 'owrrysgzkbkdgsxz'  # пароль от почты
+EMAIL_HOST_PASSWORD = 'your password'  # пароль от почты
 EMAIL_USE_SSL = True  # Яндекс использует ssl, подробнее о том, что это, почитайте в дополнительных источниках, но включать его здесь обязательно
