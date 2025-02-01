e9935eb0b3f93f973f4104d1c66a43d0089edbfa
Andrey Chirkin <chirkin.andrey377@gmail.com>
2025-02-01 10:32:40 +0300

    Modul 23 is finished

--- news_portal/news_portal/settings.py
+++ news_portal/news_portal/settings.py
@@ -207,6 +207,7 @@ CELERY_RESULT_SERIALIZER = 'json'
 
 CACHES = {
     'default': {
+        'TIMEOUT': 30,
         'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
         'LOCATION': os.path.join(BASE_DIR, 'cache_files'), # Указываем, куда будем сохранять кэшируемые файлы! Не забываем создать папку cache_files внутри папки с manage.py!
     }
