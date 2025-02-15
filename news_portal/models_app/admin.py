cf3cf73a1142292fb7dd14de0ed63ec8f5798a47
Andrey Chirkin <chirkin.andrey377@gmail.com>
2025-02-15 22:04:29 +0300

    Modul 26 is finished

--- news_portal/models_app/admin.py
+++ news_portal/models_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from models_app.models import Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'rating')
+    list_filter = ('post_type',)
+    search_fields = ('title', 'content')
+
+
+admin.site.register(Post, PostAdmin)
